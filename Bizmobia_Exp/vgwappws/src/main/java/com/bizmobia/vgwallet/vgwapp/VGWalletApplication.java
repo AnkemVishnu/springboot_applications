4d10ebb2de95b0597c95907fab7f155d25faebb3
AnkemVishnu <ankemvishnuc@gmail.com>
2020-05-27 02:21:40 +0530

    Create VGWalletApplication.java

---
+++ Bizmobia_Exp/vgwappws/src/main/java/com/bizmobia/vgwallet/vgwapp/VGWalletApplication.java
@@ -0,0 +1,65 @@
+package com.bizmobia.vgwallet.vgwapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@Configuration
+@EnableMBeanExport(defaultDomain = "third")
+@EnableJpaAuditing
+@EntityScan("com.bizmobia.vgwallet.vgwapp.models")
+@EnableAutoConfiguration
+@EnableSpringDataWebSupport
+@EnableJpaRepositories(basePackages = "com.bizmobia.vgwallet.vgwapp.dao")
+@ComponentScan(basePackages = "com.bizmobia.vgwallet.vgwapp")
+public class VGWalletApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VGWalletApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VGWalletApplication.class, args);
+    }
+
+    @Autowired
+    private Environment env;
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String urls = env.getProperty("cors.urls");
+                CorsRegistration reg = registry.addMapping("/**");
+                for (String url : urls.split(",")) {
+                    reg.allowedOrigins(url);
+                }
+            }
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/resources/**").addResourceLocations("/resources/", "classpath:/other-resources/");
+            }
+        };
+    }
+}
